ce7c80122ac9726944d93482d066b01a8fbc3952
LeeYuRi1 <dbf51561590@gmail.com>
2019-12-20 16:51:02 +0900

    project setting

---
+++ src/main/java/com/depromeet/team5/Team5Application.java
@@ -0,0 +1,13 @@
+package com.depromeet.team5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team5Application.class, args);
+    }
+
+}
