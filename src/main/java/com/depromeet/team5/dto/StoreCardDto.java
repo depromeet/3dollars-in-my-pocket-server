ad23739d930fe6d0109d21c94d85f10bed73cc98
LeeYuRi1 <dbf51561590@gmail.com>
2020-01-02 17:49:11 +0900

    거리 계산식 추가

--- src/main/java/com/depromeet/team5/dto/StoreCardDto.java
+++ src/main/java/com/depromeet/team5/dto/StoreCardDto.java
@@ -2,21 +2,25 @@ package com.depromeet.team5.dto;
 
 import com.depromeet.team5.domain.CategoryTypes;
 import com.depromeet.team5.domain.Store;
+import com.depromeet.team5.util.LocationDistance;
 import lombok.Data;
 
 @Data
 public class StoreCardDto {
+
     private Long id;
     private CategoryTypes category;
     private Long review;
-    private Long distance;
+    private Double distance;
 
     public static StoreCardDto from(Store store) {
+        LocationDistance locationDistance = new LocationDistance();
         StoreCardDto storeCardDto = new StoreCardDto();
         storeCardDto.id = store.getId();
         storeCardDto.category = store.getCategory();
         storeCardDto.review = store.getReview();
-        storeCardDto.distance = 0L;
+        storeCardDto.distance = locationDistance.distance(store.getLatitude(), store.getLongitude(), store.getLatitude(), store.getLongitude(), "meter");
         return storeCardDto;
     }
+
 }
