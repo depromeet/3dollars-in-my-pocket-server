3edc13ca79393c99d9f1def9314275555e6bf1c3
Lyle <liloyek@gmail.com>
2020-01-09 05:40:01 +0900

    Review API 인증 추가

--- src/main/java/com/depromeet/team5/controller/ReviewController.java
+++ src/main/java/com/depromeet/team5/controller/ReviewController.java
@@ -3,6 +3,7 @@ package com.depromeet.team5.controller;
 import com.depromeet.team5.dto.ReviewDto;
 import com.depromeet.team5.dto.ReviewUpdateDto;
 import com.depromeet.team5.service.ReviewService;
+import com.depromeet.team5.util.auth.Auth;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,7 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @ApiOperation("리뷰를 등록합니다. 인증이 필요한 요청입니다.")
+    @Auth
     @PostMapping("/save")
     public ResponseEntity<String> save(ReviewDto reviewDto, @RequestParam Long userId) {
         reviewService.saveReview(reviewDto, userId);
@@ -29,12 +31,14 @@ public class ReviewController {
     }
 
     @ApiOperation("사용자가 작성한 리뷰를 조회합니다. 인증이 필요한 요청입니다.")
+    @Auth
     @GetMapping("/get")
     public ResponseEntity<List<ReviewDto>> getAllByUser(@RequestParam Long userId) {
         return new ResponseEntity<>(reviewService.getAllByUser(userId), HttpStatus.OK);
     }
 
     @ApiOperation("리뷰를 수정합니다. 인증이 필요한 요청입니다.")
+    @Auth
     @PutMapping("/update")
     public ResponseEntity<String> update(ReviewUpdateDto reviewUpdateDto, @RequestParam Long reviewId) {
         reviewService.updateReview(reviewUpdateDto, reviewId);
@@ -42,6 +46,7 @@ public class ReviewController {
     }
 
     @ApiOperation("리뷰를 삭제합니다. 인증이 필요한 요청입니다.")
+    @Auth
     @DeleteMapping("/delete")
     public ResponseEntity<String> delete(@RequestParam Long reviewId) {
         reviewService.deleteReview(reviewId);
