db704def7d28d9addbd887cfd706eb597a10e433
Lyle <liloyek@gmail.com>
2020-01-08 19:17:13 +0900

    save review 추가

--- src/main/java/com/depromeet/team5/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/depromeet/team5/service/impl/ReviewServiceImpl.java
@@ -1,8 +1,11 @@
 package com.depromeet.team5.service.impl;
 
 import com.depromeet.team5.domain.Review;
+import com.depromeet.team5.domain.User;
 import com.depromeet.team5.dto.ReviewDto;
+import com.depromeet.team5.exception.UserNotFoundException;
 import com.depromeet.team5.repository.ReviewRepository;
+import com.depromeet.team5.repository.UserRepository;
 import com.depromeet.team5.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,11 +20,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
 
+    private final UserRepository userRepository;
     private final ReviewRepository reviewRepository;
 
     @Override
     public void saveReview(ReviewDto reviewDto, Long userId) {
-
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        Review review = Review.from(reviewDto, user);
+        reviewRepository.save(review);
     }
 
     @Override
