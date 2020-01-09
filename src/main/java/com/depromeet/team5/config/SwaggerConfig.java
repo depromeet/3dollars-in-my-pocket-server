858c2c1f319580c674b6ae3284f7dc3baf3fea5f
Lyle <liloyek@gmail.com>
2020-01-09 09:20:29 +0900

    swagger config에 authorization header 추가

--- src/main/java/com/depromeet/team5/config/SwaggerConfig.java
+++ src/main/java/com/depromeet/team5/config/SwaggerConfig.java
@@ -3,15 +3,16 @@ package com.depromeet.team5.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.builders.*;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
 
@@ -21,6 +22,16 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+        List<Parameter> global = new ArrayList<>();
+        global.add(new ParameterBuilder()
+                .name("Authorization")
+                .description("Access Token")
+                .parameterType("header")
+                .required(false)
+                .defaultValue("Bearer ")
+                .modelRef(new ModelRef("string"))
+                .build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
