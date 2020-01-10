9ba01a5a38620a7aa1ca9a934f8c37bd64187a96
LeeYuRi1 <dbf51561590@gmail.com>
2020-01-10 16:04:04 +0900

    id값 오류 수정

--- src/main/java/com/depromeet/team5/domain/Menu.java
+++ src/main/java/com/depromeet/team5/domain/Menu.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Entity
 public class Menu {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Long price;
--- src/main/java/com/depromeet/team5/domain/Store.java
+++ src/main/java/com/depromeet/team5/domain/Store.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Entity
 public class Store {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Float latitude;
