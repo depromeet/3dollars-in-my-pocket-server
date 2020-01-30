3ec08cd2a8bf881753eb5f026ba0889eb5c05edf
Lyle <liloyek@gmail.com>
2020-01-30 13:09:09 +0900

    review pagination 기존 page object 응답으로 되돌림

--- src/main/java/com/depromeet/team5/service/ReviewService.java
+++ src/main/java/com/depromeet/team5/service/ReviewService.java
@@ -1,13 +1,13 @@
 package com.depromeet.team5.service;
 
+import com.depromeet.team5.domain.Review;
 import com.depromeet.team5.dto.ReviewDto;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface ReviewService {
 
     void saveReview(ReviewDto reviewDto, Long userId, Long storeId);
 
-    List<ReviewDto> getAllByUser(Long userId, Pageable pageable);
+    Page<Review> getAllByUser(Long userId, Pageable pageable);
 }
--- src/main/java/com/depromeet/team5/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/depromeet/team5/service/impl/ReviewServiceImpl.java
@@ -12,12 +12,11 @@ import com.depromeet.team5.repository.UserRepository;
 import com.depromeet.team5.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -42,12 +41,8 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
-    public List<ReviewDto> getAllByUser(Long userId, Pageable pageable) {
+    public Page<Review> getAllByUser(Long userId, Pageable pageable) {
         User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
-        return reviewRepository.findByUser(user, pageable)
-                .getContent()
-                .stream()
-                .map(ReviewDto::from)
-                .collect(Collectors.toList());
+        return reviewRepository.findByUser(user, pageable);
     }
 }
