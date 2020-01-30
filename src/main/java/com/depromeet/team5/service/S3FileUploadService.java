07ab863daec15cb31bc8136e419defcdce31cd4d
Lyle <liloyek@gmail.com>
2020-01-31 08:51:46 +0900

    s3 file upload class deprecation 제거 및 코드 정리

--- src/main/java/com/depromeet/team5/service/S3FileUploadService.java
+++ src/main/java/com/depromeet/team5/service/S3FileUploadService.java
@@ -1,18 +1,23 @@
 package com.depromeet.team5.service;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.transfer.TransferManager;
-import com.amazonaws.services.s3.transfer.Upload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
@@ -20,47 +25,62 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class S3FileUploadService {
 
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
     @Value("${cloud.aws.s3.bucket.name}")
-    private String bucket;
+    private String bucketName;
 
     @Value("${cloud.aws.s3.bucket.url}")
     private String defaultUrl;
 
-    private final AmazonS3Client amazonS3Client;
+    @PostConstruct
+    private AmazonS3 initiateS3Client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
 
-    public String upload(MultipartFile uploadFile) throws IOException {
-        String origName = uploadFile.getOriginalFilename();
-        String url;
+    public String upload(MultipartFile multipartFile) throws IOException { String savedFileName = getSavedFileName(Objects.requireNonNull(multipartFile.getOriginalFilename()));
         try {
-            final String ext = origName.substring(origName.lastIndexOf('.'));
-            final String saveFileName = getUuid() + ext;
-            File file = new File(System.getProperty("user.dir") + saveFileName);
-            uploadFile.transferTo(file);
-            uploadOnS3(saveFileName, file);
-            url = defaultUrl + saveFileName;
+            File file = convertMultiPartToFile(multipartFile);
+            uploadFileToS3(savedFileName, file);
             file.delete();
-        }catch (StringIndexOutOfBoundsException e) {
-            url = null;
+        } catch (StringIndexOutOfBoundsException e) {
+            return null;
         }
-        return url;
+        return defaultUrl + savedFileName;
     }
 
     private static String getUuid() {
         return UUID.randomUUID().toString().replaceAll("-", "");
     }
 
-    private void uploadOnS3(final String fileName, final File file) {
-        final TransferManager transferManager = new TransferManager(this.amazonS3Client);
+    private static String getSavedFileName(String origName) {
+        return getUuid() + origName.substring(origName.lastIndexOf('.'));
+    }
 
-        final PutObjectRequest request = new PutObjectRequest(bucket, fileName, file);
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convertedFile;
+    }
 
-        final Upload upload = transferManager.upload(request);
-
+    private void uploadFileToS3(final String fileName, final File file) {
         try {
-            upload.waitForCompletion();
-        } catch (AmazonClientException amazonClientException) {
-            log.error(amazonClientException.getMessage());
-        } catch (InterruptedException e) {
+            AmazonS3 s3Client = initiateS3Client();
+            s3Client.putObject(new PutObjectRequest(bucketName, fileName, file));
+        }catch(AmazonServiceException e) {
             log.error(e.getMessage());
         }
     }
