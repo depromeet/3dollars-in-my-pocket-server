6d5b09cb3474a2e43d2d215da98580acdeb4d704
Lyle <liloyek@gmail.com>
2020-01-31 01:53:21 +0900

    review pom dto에서 reviewDto -> review 로 수정

--- src/main/java/com/depromeet/team5/dto/ReviewPomDto.java
+++ src/main/java/com/depromeet/team5/dto/ReviewPomDto.java
@@ -1,16 +1,18 @@
 package com.depromeet.team5.dto;
 
+import com.depromeet.team5.domain.Review;
+
 import java.util.List;
 
 public class ReviewPomDto {
 
-    private List<ReviewDto> content;
+    private List<Review> content;
 
     private Long totalElements;
 
     private int totalPages;
 
-    public static ReviewPomDto from(List<ReviewDto> content, Long totalElements, int totalPages) {
+    public static ReviewPomDto from(List<Review> content, Long totalElements, int totalPages) {
         ReviewPomDto ReviewPomDto = new ReviewPomDto();
         ReviewPomDto.content = content;
         ReviewPomDto.totalElements = totalElements;
