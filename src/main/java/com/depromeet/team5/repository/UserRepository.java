62ca59d9ba753df7ccc2e280bdab3172bd4c5234
전해성 <seong0428@gmail.com>
2020-12-11 01:31:44 +0900

    fix: 닉네임 변경시 쿼리 에러 발생하는 현상

--- src/main/java/com/depromeet/team5/repository/UserRepository.java
+++ src/main/java/com/depromeet/team5/repository/UserRepository.java
@@ -5,12 +5,13 @@ import com.depromeet.team5.domain.user.User;
 import com.depromeet.team5.domain.user.UserStatusType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySocialIdAndSocialType(String socialId, SocialTypes socialType);
 
-    boolean existsByNameLike(String nickname);
+    List<User> findByNameAndStatus(String name, UserStatusType type);
 
     Optional<User> findByIdAndStatus(Long userId, UserStatusType type);
 }
