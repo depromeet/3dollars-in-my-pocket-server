06fd2f8bdde0c6446501a6592c5cbb1cc3f0b0b6
LeeYuRi1 <dbf51561590@gmail.com>
2021-01-22 16:55:44 +0900

    feat: image 추가, 삭제 테스트 코드 작성

--- interfaces/src/test/java/com/depromeet/team5/integration/ReviewTest.java
+++ interfaces/src/test/java/com/depromeet/team5/integration/ReviewTest.java
@@ -7,7 +7,6 @@ import com.depromeet.team5.integration.api.StoreTestController;
 import com.depromeet.team5.integration.api.UserTestController;
 import com.depromeet.team5.repository.ReviewRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
