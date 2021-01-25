bcbdaff0efcdbeea6b66bb911a3a1554c0c50d35
Lyle <calends99@gmail.com>
2021-01-25 17:05:52 +0900

    refactor: 코드 정렬

--- domain/src/main/java/com/depromeet/team5/exception/ApplicationException.java
+++ domain/src/main/java/com/depromeet/team5/exception/ApplicationException.java
@@ -1,7 +1,8 @@
 package com.depromeet.team5.exception;
 
 public abstract class ApplicationException extends RuntimeException implements ResultCodeSupport {
-    public ApplicationException() { }
+    public ApplicationException() {
+    }
 
     public ApplicationException(String message) {
         super(message);
