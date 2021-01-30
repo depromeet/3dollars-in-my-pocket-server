077da44071c8531a7236e803fd49b325a869f9b1
Haeseong Jeon <seong0428@gmail.com>
2021-01-30 17:13:34 +0900

    feat: 로마자 표기 적용

--- domain/src/main/java/com/depromeet/team5/domain/store/CategoryType.java
+++ domain/src/main/java/com/depromeet/team5/domain/store/CategoryType.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
  * 초기값(붕어빵, 타코야끼, 계란빵, 호떡) 과의 일관성을 위해 로마자 표기법을 사용합니다.
  * 단, 외국어 및 알파벳으로 표기 가능한 외래어는 원래 이름을 그대로 사용합니다.
  * `@Enumerated(EnumType.STRING)` 으로 저장되어있어서 기존 값 migration 하는 것은 까다로움.
+ * @see <a href="http://roman.cs.pusan.ac.kr/">http://roman.cs.pusan.ac.kr/</a>
  */
 @RequiredArgsConstructor
 public enum CategoryType {
@@ -16,15 +17,15 @@ public enum CategoryType {
     HOTTEOK("호떡"),
     EOMUK("어묵"),
     GUNGOGUMA("군고구마"),
-    TTEOKBOKI("떡볶이"),
+    TTEOKBOKKI("떡볶이"),
     TTANGKONGPPANG("땅콩빵"),
     GUNOKSUSU("군옥수수"),
     DAKKKOCHI("닭꼬치"),
     TOAST("토스트"),
     WAFFLE("와플"),
-    GUKHWAPPANG("국화빵"),
+    GUKWAPPANG("국화빵"),
     SUNDAE("순대"),
     ;
 
-    private final String category;
+    private final String description;
 }
