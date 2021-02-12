44870c4fdb997d5eefc77694f63b9cb8d72ee859
Haeseong Jeon <seong0428@gmail.com>
2021-02-12 21:28:52 +0900

    feat: 가게 삭제 사유 추가

--- domain/src/main/java/com/depromeet/team5/domain/store/DeleteReasonType.java
+++ domain/src/main/java/com/depromeet/team5/domain/store/DeleteReasonType.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 public enum DeleteReasonType {
     NOSTORE("없어진 가게에요"),
     WRONGNOPOSITION("위치가 잘못됐어요"),
-    OVERLAPSTORE("중복 제보된 가게에요");
+    OVERLAPSTORE("중복 제보된 가게에요"),
+    WRONG_CONTENT("부적절한 내용이 있어요"),
+    ;
 
     private final String reason;
 }
