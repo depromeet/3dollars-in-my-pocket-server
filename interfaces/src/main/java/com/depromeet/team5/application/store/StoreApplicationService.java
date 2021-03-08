2c967eadc3bcf0bcf108a5d0011a9044b0503e32
Lyle <calends99@gmail.com>
2021-03-09 01:49:48 +0900

    feat: 위도, 경도로 근처 가게 조회시 검색 반경을 2km로 제한하도록 수정

--- interfaces/src/main/java/com/depromeet/team5/application/store/StoreApplicationService.java
+++ interfaces/src/main/java/com/depromeet/team5/application/store/StoreApplicationService.java
@@ -24,9 +24,12 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class StoreApplicationService {
+
     private final StoreService storeService;
     private final StoreAssembler storeAssembler;
     private final S3FileUploadService s3FileUploadService;
+
+    private static final double DISTANCE_LIMIT = 2.0;
 
     @Transactional(readOnly = true)
     public StoreDetailDto getStoreDetail(Long storeId, Double latitude, Double longitude) {
@@ -34,6 +37,11 @@ public class StoreApplicationService {
         return storeAssembler.toStoreDetailDto(store, latitude, longitude);
     }
 
+    /**
+     * 해당 서비스 메소드가 내부적으로 사용하는 쿼리의 성능 이슈가 존재하여
+     * 최대 검색 반경을 2km로 제한합니다.
+     * 추후 쿼리 최적화 혹은 새 API 개발을 통해 고도화할 예정입니다.
+     */
     @Transactional(readOnly = true)
     public List<StoreResponse> getStoresByLocationAndDistance(Location userLocation,
                                                               Location mapLocation,
@@ -42,6 +50,8 @@ public class StoreApplicationService {
         Assert.notNull(userLocation, "'userLocation' must not be null");
         Assert.notNull(mapLocation, "'mapLocation' must not be null");
         Assert.notNull(distance, "'distance' must not be null");
+
+        if (distance > DISTANCE_LIMIT) distance = DISTANCE_LIMIT;
 
         return storeService.getStoresByLocationAndDistance(mapLocation, distance).stream()
                 .map(it -> storeAssembler.toStoreResponse(it, userLocation))
@@ -97,7 +107,7 @@ public class StoreApplicationService {
     /**
      * 가게에 이미지 1개 추가
      *
-     * @param storeId 가게 식별자
+     * @param storeId          가게 식별자
      * @param imageUploadValue 이미지 업로드 정보
      * @return 가게에 추가된 이미지 정보
      */
